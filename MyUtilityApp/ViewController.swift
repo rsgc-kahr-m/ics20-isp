0e8066d1d9faab87decf6b2a7ec79548504adbc9
Student <student@macbook-pro-867-kahr-landol.local>
2017-01-12 10:04:43 -0500

    Added interface and connected to code.

--- MyUtilityApp/ViewController.swift
+++ MyUtilityApp/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var friendNames: UITextField!
+    
+    @IBOutlet weak var friendAmounts: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
