691f56f063a70b57ef60ae76b6cdb43095fe261d
Student <student@macbook-pro-867-kahr-landol.local>
2017-01-16 15:40:50 -0500

    Tried to make fields respond to edits.

--- MyUtilityApp/ViewController.swift
+++ MyUtilityApp/ViewController.swift
@@ -20,25 +20,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // Make it so that each time a text box is edited, the contents of all text boxes are saved
-        for subview in self.view.subviews {
-            if let textField = subview as? UITextField {
-                textField.addTarget(self, action: #selector(self.saveAmountOwed), for: .editingDidEnd)
-                
-            }
-        }
+
     
     }
 
+    override func viewDidLayoutSubviews() {
+        // Make it so that each time a text box is edited, the contents of all text boxes are saved
+        for subview in self.view.subviews {
+            if let textField = subview as? UITextField {
+                textField.addTarget(self, action: #selector(self.saveAmountOwed), for: .editingChanged)
+                
+            }
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     func saveAmountOwed() {
+        print("about to list all the friends")
         for (position, friendName) in friendNames.enumerated() {
             print(position)
-            print(friendName)
+            print(friendName.text)
         }
     }
 
