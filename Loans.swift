231b1f5af909d58e79743aabfa96756f786b9cd2
matias <matias@gordon.local>
2017-01-16 16:17:03 -0500

    Added a model to represent a loan.

---
+++ Loans.swift
@@ -0,0 +1,28 @@
+//
+//  Loans.swift
+//  MyUtilityApp
+//
+//  Created by matias on 2017-01-16.
+//  Copyright © 2017 matias Kahr. All rights reserved.
+//
+
+import UIKit
+
+
+
+class Loan {
+
+    
+    var name : String
+    
+    var amount : Double
+    
+    init(name: String, amount: Double) {
+        
+        self.name = name
+        self.amount = amount
+        
+    }
+    
+    
+}
