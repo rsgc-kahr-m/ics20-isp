940300eb70290c54c544bf2d4ec108af0037ea5f
matias <matias@gordon.local>
2017-01-19 15:55:10 -0500

    Changed loans class to support saving data to disk

--- Loans.swift
+++ Loans.swift
@@ -8,20 +8,43 @@
 
 import UIKit
 
+class Loan : NSObject, NSCoding {
 
-
-class Loan {
-
-    
+    //MARK: properties
     var name : String
-    
     var amount : Double
     
-    init(name: String, amount: Double) {
+    //MARK: Paths for archiving data across sessions
+    static var DocumentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    static let ArchiveURL = DocumentsDirectory[0].appendPathComponent("Loans")
+    
+    //MARK: Types
+    struct PropertyKey {
+        static let nameKey = "name"
+        static let amountKey = "amounts"
+        
+    }
+    
+    //This is what allows an instance of a loan to be initialized with the name and amount owed
+     init(name: String, amount: Double) {
         
         self.name = name
         self.amount = amount
         
+    }
+    
+    //This lets me save information about a loan on a disk
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.nameKey)
+        aCoder.encode(amount, forKey: PropertyKey.amountKey)
+    }
+    
+    //This function allows me to load information
+    required convenience init?(coder aDecoder : NSCoder) {
+        let name = aDecoder.decodeObject(forKey: PropertyKey.nameKey) as! String
+        let amount = aDecoder.decodeObject(forKey: PropertyKey.amountKey) as! Double
+        
+        self.init(name: name, amount: amount)
     }
     
     
